c87f39f012dfc49a1749a9655aa4070aa36b9de5
celic <cceli@codequarry.net>
2012-12-10 22:14:31 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Topsoil::Application.config.secret_token = '9ecbd0d8d09340f18c1c69fc9d73b9f384bba854ef77b95b31a981fc0f8bf036fd9f42f743544f366534fa0666965e823b7b4f2bb63820b6203bfac20cb0c959'
