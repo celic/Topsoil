60b4048d7faafdc6eb65d8294c739e8a84c6f04e
celic <cceli@codequarry.net>
2012-12-15 22:37:00 -0500

    Deletes public.html

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(:version => 20121212042759) do
 
   create_table "users", :force => true do |t|
-    t.string   "name"
+    t.string   "name",                   :default => "", :null => false
     t.string   "email",                  :default => "", :null => false
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(:version => 20121212042759) do
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["name"], :name => "index_users_on_name"
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
